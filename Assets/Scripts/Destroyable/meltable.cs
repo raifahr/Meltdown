67650e64adfde51bce4e766e15774a2b32e91423
advayy <81641671+advayy@users.noreply.github.com>
2025-07-19 02:47:41 -0700

    steam on melt

--- Assets/Scripts/Destroyable/meltable.cs
+++ Assets/Scripts/Destroyable/meltable.cs
@@ -4,13 +4,24 @@ public class meltable : MonoBehaviour
 {
     public float shrinkRate = 0.8f;
     public float scaleToDestroyAt = 0.2f;
-
+    public ParticleSystem steamParticlePrefab;
+    private ParticleSystem steamParticlesInstance;
     private bool isMelting = false;
 
     void Update()
     {
         if (isMelting)
         {
+            if (steamParticlesInstance == null)
+            {
+                steamParticlesInstance = Instantiate(steamParticlePrefab, transform.position, Quaternion.identity, transform);
+                steamParticlesInstance.Play();
+            }
+            else if (!steamParticlesInstance.isPlaying)
+            {
+                steamParticlesInstance.Play();
+            }
+
             float shrinkAmount = shrinkRate * Time.deltaTime;
             transform.localScale -= new Vector3(shrinkAmount, shrinkAmount, shrinkAmount);
 
@@ -19,11 +30,17 @@ public class meltable : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+        else
+        {
+            if (steamParticlesInstance != null && steamParticlesInstance.isPlaying)
+            {
+                steamParticlesInstance.Stop();
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Flame"))
         {
             isMelting = true;
